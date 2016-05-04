5cf7e583887e955794f75b9e89df2544371e5c0f
Mike <@>
2016-05-04 08:06:36 +0200

    test

---
+++ app/src/main/java/com/example/iuliu/androiddb/AddInfo.java
@@ -0,0 +1,94 @@
+package com.example.iuliu.androiddb;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+
+public class AddInfo extends AppCompatActivity {
+    EditText Name,Password,Random;
+    String name,password,random;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_info_layout);
+        Name=(EditText)findViewById(R.id.et_name);
+        Password=(EditText)findViewById(R.id.et_password);
+        Random=(EditText)findViewById(R.id.et_random);
+    }
+    public void saveInfo(View view)
+    {
+        name=Name.getText().toString();
+        password=Password.getText().toString();
+        random=Random.getText().toString();
+        BackgroundTask backgroundTask = new BackgroundTask();
+        backgroundTask.execute(name,password,random);
+
+    }
+    class BackgroundTask extends AsyncTask<String,Void,String>
+    {
+        String add_info_url;
+        @Override
+        protected void onPreExecute() {
+            add_info_url= "http://mybarter.net16.net/add_info.php";
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... args) {
+            String name,password,random;
+            name=args[0];
+            password=args[1];
+            random=args[2];
+            try {
+                URL url =new URL(add_info_url);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoOutput(true);
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+                String data_string = URLEncoder.encode("name","UTF-8")+"="+URLEncoder.encode(name,"UTF-8")+"&"+
+                        URLEncoder.encode("password","UTF-8")+"="+URLEncoder.encode(password,"UTF-8")+"&"+
+                        URLEncoder.encode("random","UTF-8")+"="+URLEncoder.encode(random,"UTF-8");
+                bufferedWriter.write(data_string);
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                outputStream.close();
+                InputStream inputStream=httpURLConnection.getInputStream();
+                inputStream.close();
+                httpURLConnection.disconnect();
+                return "Data insert";
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Toast.makeText(getApplicationContext(),result,Toast.LENGTH_LONG).show();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/iuliu/androiddb/ViewItems.java
@@ -0,0 +1,81 @@
+package com.example.iuliu.androiddb;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class ViewItems extends AppCompatActivity {
+    String JSON_STRING;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_items_layout);
+
+
+    }
+
+    public void getJSON(View view) {
+        BackgroundTask backgroundTask =new BackgroundTask();
+        backgroundTask.execute();
+    }
+
+    class BackgroundTask extends AsyncTask<String, Void, String> {
+        String json_url;
+        @Override
+        protected void onPreExecute()
+        {
+            json_url = "http://mybarter.net16.net/json_data.php";
+        }
+
+        @Override
+        protected String doInBackground(String... params)
+        {
+            try
+            {
+                URL url = new URL(json_url);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                StringBuilder stringBuilder = new StringBuilder();
+                while((JSON_STRING = bufferedReader.readLine())!= null)
+                {
+                stringBuilder.append(JSON_STRING+"\n");
+                }
+                bufferedReader.close();
+                inputStream.close();
+                httpURLConnection.disconnect();
+                return stringBuilder.toString().trim();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values)
+        {
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected void onPostExecute(String result)
+        {
+            TextView textView = (TextView)findViewById(R.id.textView);
+            textView.setText(result);
+           // Toast.makeText(getApplicationContext(), result, Toast.LENGTH_LONG).show();
+        }
+    }
+}
