e65903fe6ee54a2eda9058093a94022dd61d6d52
Erik <Deciver_89@hotmail.com>
2016-05-12 08:00:48 +0200

    simple test

--- app/src/test/java/com/example/iuliu/androiddb/ExampleUnitTest.java
+++ app/src/test/java/com/example/iuliu/androiddb/ExampleUnitTest.java
@@ -1,13 +1,19 @@
 package com.example.iuliu.androiddb;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 
 public class ExampleUnitTest {
+    @Before
+    public void createLogin(){
+
+    }
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
     }
+
 }
