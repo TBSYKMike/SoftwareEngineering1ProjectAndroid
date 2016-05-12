3fc81c795f2edb93ee827f4e5a01a55ed7113421
Erik <Deciver_89@hotmail.com>
2016-05-12 07:03:16 +0200

    simple test

--- app/src/androidTest/java/com/example/iuliu/androiddb/ApplicationTest.java
+++ app/src/androidTest/java/com/example/iuliu/androiddb/ApplicationTest.java
@@ -7,7 +7,18 @@ import android.test.ApplicationTestCase;
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
+    private Application application;
+
     public ApplicationTest() {
         super(Application.class);
+    }
+    protected void setUp()throws Exception{
+        super.setUp();
+        createApplication();
+        application = getApplication();
+    }
+    public void TestExist(){
+
+        assertNotNull(application);
     }
 }
