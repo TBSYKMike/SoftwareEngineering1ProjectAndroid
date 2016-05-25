9479ae55741923b5ba7432ac39bdbfff0b6fb4b1
IuliuNicolae <iuliunicolaebarcan@gmail.com>
2016-05-25 12:29:18 +0200

    secret Key

--- app/src/main/java/com/example/iuliu/androiddb/MySecretKey.java
+++ app/src/main/java/com/example/iuliu/androiddb/MySecretKey.java
@@ -4,19 +4,41 @@ package com.example.iuliu.androiddb;
  * Created by Iuliu on 2016-05-08.
  */
 public class MySecretKey {
-    private static final long NUMBER1 = 9907;
-    private static final long NUMBER2 = 6247;
-    private static final long NUMBER3 = 4729;
+    private static final long NUMBER1 = 9909567478476247L;
+    private static final long NUMBER2 = 6247472905479567L;
+    private static final long NUMBER3 = 4725259948019567L;
+    private static final long NUMBER4 = 9567444454665655L;
+    long a = NUMBER1;
+    long b = NUMBER2;
+    long c = NUMBER3;
+    long d = NUMBER4;
+    long temp1 = (a * c) + b;
+    long temp2 = a * (b - c);
+    long result3 = temp1 % b;
 
-    public long getNumber() {
+    long result4=temp2 % c;
+    public String getNumber1() {
+
+        long temp1 = (a * c) + b;
+        long temp2 = a * (b - c);
+        long result3 = temp1 % b;
+
+        long result4=temp2 % c;
+        String s=Long.toString(result3);
+
+
+        return s;
+    }
+    public String getNumber2() {
         long a = NUMBER1;
         long b = NUMBER2;
         long c = NUMBER3;
+        long d = NUMBER4;
         long temp1 = (a * c) + b;
         long temp2 = a * (b - c);
-        long d = temp1 % temp2;
-        System.out.println(d);
+        long result4 = temp2 % b;
 
-        return d;
+        String s=Long.toString(result4);
+        return s;
     }
 }
